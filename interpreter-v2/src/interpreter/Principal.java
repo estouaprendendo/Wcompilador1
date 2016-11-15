e009e41110ef46cda8edc7525dbc8a7a4d37ab14
Estou Aprendendo <estouaprendendo@gmail.com>
2016-11-15 13:47:19 -0200

    2

--- interpreter-v2/src/interpreter/Principal.java
+++ interpreter-v2/src/interpreter/Principal.java
@@ -5,5 +5,24 @@ import java.io.IOException;
 public class Principal {
 	public static void main(String[] args) throws IOException{
 		
+		AnalisadorLexico analisadorLexico = new AnalisadorLexico();
+		analisadorLexico.analisar();
+		
+		if (analisadorLexico.temErros()) {
+			analisadorLexico.mostraErros();
+			System.exit(0);
+		}
+		
+		AnalisadorSintatico analisadorSintatico = new AnalisadorSintatico(analisadorLexico.getTokens());
+		analisadorSintatico.analisar();
+
+		if (analisadorSintatico.temErros()) {
+			analisadorSintatico.mostraErros();
+			System.exit(0);
+		}
+		
+		analisadorSintatico.mostraArvore();
+		
+		
 	}
 }
